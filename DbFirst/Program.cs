56d0cf05276d583ae81e7a5f4cbcd0fcae2944ef
prakaski <kiran.prakash@sunquestinfo.com>
2021-11-20 20:56:19 +0530

    Modified type of Level property of course entity in model from byte to enum type Level.

--- DbFirst/Program.cs
+++ DbFirst/Program.cs
@@ -6,16 +6,17 @@ using System.Threading.Tasks;
 
 namespace DbFirst
 {
+    public enum Level : byte
+    {
+        Beginner = 1,
+        Intermediate = 2,
+        Advanced = 3
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var dbContext = new PlutoDbContext();
-            var courses = dbContext.GetCourses();
-            foreach(var c in courses)
-            {
-                Console.WriteLine(c.Title);
-            }
+
         }
     }
 }
